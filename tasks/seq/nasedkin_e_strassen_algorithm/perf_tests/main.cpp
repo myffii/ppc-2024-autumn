a7bb1044eaf1f1caf33631f01a61bb87218194ab
myffii <132840123+myffii@users.noreply.github.com>
2024-12-17 14:47:54 +0300

    1

--- tasks/seq/nasedkin_e_strassen_algorithm/perf_tests/main.cpp
+++ tasks/seq/nasedkin_e_strassen_algorithm/perf_tests/main.cpp
@@ -5,51 +5,61 @@
 #include "seq/nasedkin_e_strassen_algorithm/include/ops_seq.hpp"
 
 TEST(nasedkin_e_strassen_algorithm_seq, test_pipeline_run) {
-    auto taskData = std::make_shared<ppc::core::TaskData>();
-    taskData->inputs_count.push_back(64);
+  int matrixSize = 8;
 
-    auto strassenTask = std::make_shared<nasedkin_e_strassen_algorithm::StrassenAlgorithmSeq>(taskData);
+  auto strassenTask = std::make_shared<nasedkin_e_strassen_algorithm::StrassenAlgorithmSeq>(matrixSize);
 
-    ASSERT_TRUE(strassenTask->validation()) << "Validation failed for valid input";
+  std::vector<std::vector<double>> matrixA;
+  std::vector<std::vector<double>> matrixB;
+  nasedkin_e_strassen_algorithm::StrassenAlgorithmSeq::generate_random_matrix(matrixSize, matrixA);
+  nasedkin_e_strassen_algorithm::StrassenAlgorithmSeq::generate_random_matrix(matrixSize, matrixB);
+  strassenTask->set_matrices(matrixA, matrixB);
 
-    strassenTask->pre_processing();
-    strassenTask->run();
-    strassenTask->post_processing();
+  ASSERT_TRUE(strassenTask->validation()) << "Validation failed for valid input";
 
-    auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-    perfAttr->num_running = 10;
-    const boost::mpi::timer current_timer;
-    perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  strassenTask->pre_processing();
+  strassenTask->run();
+  strassenTask->post_processing();
 
-    auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const boost::mpi::timer current_timer;
+  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
 
-    auto perfAnalyzer = std::make_shared<ppc::core::Perf>(strassenTask);
-    perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
-    ppc::core::Perf::print_perf_statistic(perfResults);
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(strassenTask);
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+
+  ppc::core::Perf::print_perf_statistic(perfResults);
 }
 
 TEST(nasedkin_e_strassen_algorithm_seq, test_task_run) {
-    auto taskData = std::make_shared<ppc::core::TaskData>();
-    taskData->inputs_count.push_back(64);
+  int matrixSize = 8;
 
-    auto strassenTask = std::make_shared<nasedkin_e_strassen_algorithm::StrassenAlgorithmSeq>(taskData);
+  auto strassenTask = std::make_shared<nasedkin_e_strassen_algorithm::StrassenAlgorithmSeq>(matrixSize);
 
-    ASSERT_TRUE(strassenTask->validation()) << "Validation failed for valid input";
+  std::vector<std::vector<double>> matrixA;
+  std::vector<std::vector<double>> matrixB;
+  nasedkin_e_strassen_algorithm::StrassenAlgorithmSeq::generate_random_matrix(matrixSize, matrixA);
+  nasedkin_e_strassen_algorithm::StrassenAlgorithmSeq::generate_random_matrix(matrixSize, matrixB);
+  strassenTask->set_matrices(matrixA, matrixB);
 
-    strassenTask->pre_processing();
-    strassenTask->run();
-    strassenTask->post_processing();
+  ASSERT_TRUE(strassenTask->validation()) << "Validation failed for valid input";
 
-    auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-    perfAttr->num_running = 10;
-    const boost::mpi::timer current_timer;
-    perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  strassenTask->pre_processing();
+  strassenTask->run();
+  strassenTask->post_processing();
 
-    auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const boost::mpi::timer current_timer;
+  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
 
-    auto perfAnalyzer = std::make_shared<ppc::core::Perf>(strassenTask);
-    perfAnalyzer->task_run(perfAttr, perfResults);
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
-    ppc::core::Perf::print_perf_statistic(perfResults);
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(strassenTask);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+
+  ppc::core::Perf::print_perf_statistic(perfResults);
 }
