dc2b5750584161bbe821efa5d7548014849a4e26
myffii <132840123+myffii@users.noreply.github.com>
2024-12-29 06:00:13 +0300

    1

--- tasks/mpi/nasedkin_e_strassen_algorithm/func_tests/main.cpp
+++ tasks/mpi/nasedkin_e_strassen_algorithm/func_tests/main.cpp
@@ -123,6 +123,54 @@ TEST(nasedkin_e_strassen_algorithm_mpi, Test_2x2) {
   ASSERT_EQ(resultSeq, resultParallel);
 }
 
+TEST(nasedkin_e_strassen_algorithm_mpi, Test_3x3) {
+boost::mpi::communicator world;
+
+int matrixSize = 3;
+std::vector<double> matrixA;
+std::vector<double> matrixB;
+if (world.rank() == 0) {
+matrixA = generateRandomMatrix(matrixSize);
+matrixB = generateRandomMatrix(matrixSize);
+}
+std::vector<double> resultParallel(matrixSize * matrixSize, 0.0);
+std::vector<double> resultSeq(matrixSize * matrixSize, 0.0);
+
+std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+
+if (world.rank() == 0) {
+taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA.data()));
+taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB.data()));
+taskDataSeq->inputs_count.emplace_back(matrixA.size());
+taskDataSeq->inputs_count.emplace_back(matrixB.size());
+taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(resultSeq.data()));
+taskDataSeq->outputs_count.emplace_back(resultSeq.size());
+
+nasedkin_e_strassen_algorithm::StrassenAlgorithmSEQ testMpiTaskSeq(taskDataSeq);
+ASSERT_TRUE(testMpiTaskSeq.validation());
+ASSERT_TRUE(testMpiTaskSeq.pre_processing());
+ASSERT_TRUE(testMpiTaskSeq.run());
+ASSERT_TRUE(testMpiTaskSeq.post_processing());
+}
+
+std::shared_ptr<ppc::core::TaskData> taskDataParallel = std::make_shared<ppc::core::TaskData>();
+
+if (world.rank() == 0) {
+taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA.data()));
+taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB.data()));
+taskDataParallel->inputs_count.emplace_back(matrixA.size());
+taskDataParallel->inputs_count.emplace_back(matrixB.size());
+taskDataParallel->outputs.emplace_back(reinterpret_cast<uint8_t*>(resultParallel.data()));
+taskDataParallel->outputs_count.emplace_back(resultParallel.size());
+}
+nasedkin_e_strassen_algorithm::StrassenAlgorithmMPI testMpiTaskParallel(taskDataParallel);
+ASSERT_TRUE(testMpiTaskParallel.validation());
+ASSERT_TRUE(testMpiTaskParallel.pre_processing());
+ASSERT_TRUE(testMpiTaskParallel.run());
+ASSERT_TRUE(testMpiTaskParallel.post_processing());
+ASSERT_EQ(resultSeq, resultParallel);
+}
+
 TEST(nasedkin_e_strassen_algorithm_mpi, Test_4x4) {
   boost::mpi::communicator world;
 
@@ -172,6 +220,54 @@ TEST(nasedkin_e_strassen_algorithm_mpi, Test_4x4) {
   ASSERT_EQ(resultSeq, resultParallel);
 }
 
+TEST(nasedkin_e_strassen_algorithm_mpi, Test_7x7) {
+boost::mpi::communicator world;
+
+int matrixSize = 7;
+std::vector<double> matrixA;
+std::vector<double> matrixB;
+if (world.rank() == 0) {
+matrixA = generateRandomMatrix(matrixSize);
+matrixB = generateRandomMatrix(matrixSize);
+}
+std::vector<double> resultParallel(matrixSize * matrixSize, 0.0);
+std::vector<double> resultSeq(matrixSize * matrixSize, 0.0);
+
+std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+
+if (world.rank() == 0) {
+taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA.data()));
+taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB.data()));
+taskDataSeq->inputs_count.emplace_back(matrixA.size());
+taskDataSeq->inputs_count.emplace_back(matrixB.size());
+taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(resultSeq.data()));
+taskDataSeq->outputs_count.emplace_back(resultSeq.size());
+
+nasedkin_e_strassen_algorithm::StrassenAlgorithmSEQ testMpiTaskSeq(taskDataSeq);
+ASSERT_TRUE(testMpiTaskSeq.validation());
+ASSERT_TRUE(testMpiTaskSeq.pre_processing());
+ASSERT_TRUE(testMpiTaskSeq.run());
+ASSERT_TRUE(testMpiTaskSeq.post_processing());
+}
+
+std::shared_ptr<ppc::core::TaskData> taskDataParallel = std::make_shared<ppc::core::TaskData>();
+
+if (world.rank() == 0) {
+taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA.data()));
+taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB.data()));
+taskDataParallel->inputs_count.emplace_back(matrixA.size());
+taskDataParallel->inputs_count.emplace_back(matrixB.size());
+taskDataParallel->outputs.emplace_back(reinterpret_cast<uint8_t*>(resultParallel.data()));
+taskDataParallel->outputs_count.emplace_back(resultParallel.size());
+}
+nasedkin_e_strassen_algorithm::StrassenAlgorithmMPI testMpiTaskParallel(taskDataParallel);
+ASSERT_TRUE(testMpiTaskParallel.validation());
+ASSERT_TRUE(testMpiTaskParallel.pre_processing());
+ASSERT_TRUE(testMpiTaskParallel.run());
+ASSERT_TRUE(testMpiTaskParallel.post_processing());
+ASSERT_EQ(resultSeq, resultParallel);
+}
+
 TEST(nasedkin_e_strassen_algorithm_mpi, Test_8x8) {
   boost::mpi::communicator world;
 
@@ -219,6 +315,54 @@ TEST(nasedkin_e_strassen_algorithm_mpi, Test_8x8) {
   ASSERT_TRUE(testMpiTaskParallel.post_processing());
 
   ASSERT_EQ(resultSeq, resultParallel);
+}
+
+TEST(nasedkin_e_strassen_algorithm_mpi, Test_10x10) {
+boost::mpi::communicator world;
+
+int matrixSize = 10;
+std::vector<double> matrixA;
+std::vector<double> matrixB;
+if (world.rank() == 0) {
+matrixA = generateRandomMatrix(matrixSize);
+matrixB = generateRandomMatrix(matrixSize);
+}
+std::vector<double> resultParallel(matrixSize * matrixSize, 0.0);
+std::vector<double> resultSeq(matrixSize * matrixSize, 0.0);
+
+std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+
+if (world.rank() == 0) {
+taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA.data()));
+taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB.data()));
+taskDataSeq->inputs_count.emplace_back(matrixA.size());
+taskDataSeq->inputs_count.emplace_back(matrixB.size());
+taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(resultSeq.data()));
+taskDataSeq->outputs_count.emplace_back(resultSeq.size());
+
+nasedkin_e_strassen_algorithm::StrassenAlgorithmSEQ testMpiTaskSeq(taskDataSeq);
+ASSERT_TRUE(testMpiTaskSeq.validation());
+ASSERT_TRUE(testMpiTaskSeq.pre_processing());
+ASSERT_TRUE(testMpiTaskSeq.run());
+ASSERT_TRUE(testMpiTaskSeq.post_processing());
+}
+
+std::shared_ptr<ppc::core::TaskData> taskDataParallel = std::make_shared<ppc::core::TaskData>();
+
+if (world.rank() == 0) {
+taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA.data()));
+taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB.data()));
+taskDataParallel->inputs_count.emplace_back(matrixA.size());
+taskDataParallel->inputs_count.emplace_back(matrixB.size());
+taskDataParallel->outputs.emplace_back(reinterpret_cast<uint8_t*>(resultParallel.data()));
+taskDataParallel->outputs_count.emplace_back(resultParallel.size());
+}
+nasedkin_e_strassen_algorithm::StrassenAlgorithmMPI testMpiTaskParallel(taskDataParallel);
+ASSERT_TRUE(testMpiTaskParallel.validation());
+ASSERT_TRUE(testMpiTaskParallel.pre_processing());
+ASSERT_TRUE(testMpiTaskParallel.run());
+ASSERT_TRUE(testMpiTaskParallel.post_processing());
+ASSERT_EQ(resultSeq, resultParallel);
 }
 
 TEST(nasedkin_e_strassen_algorithm_mpi, Test_16x16) {
